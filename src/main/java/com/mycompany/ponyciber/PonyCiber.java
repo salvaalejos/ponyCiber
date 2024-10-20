ae7b2b0b0c3ec83f363524a504b8ef20855a55f2
salvaalejos <alejossalvador@gmail.com>
2024-10-20 15:20:49 -0600

    Tarea TAP ciber con hilos

---
+++ src/main/java/com/mycompany/ponyciber/PonyCiber.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ponyciber;
+
+/**
+ *
+ * @author hfyh
+ */
+public class PonyCiber {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
